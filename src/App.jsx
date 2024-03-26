f7226c513eff891ffb6a6fa4319e1dcc04329580
tigerkaplane <husniyeerparun@gmail.com>
2024-03-26 03:54:34 +0000

    update

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import "./App.css";
+import { Route } from "react-router-dom";
+
+function App() {
+  return (
+    <div>
+
+    
+        <Routes>
+          <Route path="/" element={<Home />} />
+
+        </Routes>
+
+    </div>
+  );
+}
+
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+//React router component
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
 import './index.css'
+
+const router = createBrowserRouter([
+
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
