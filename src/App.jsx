ef43f83594cebe043993a3ffe75e0fc56eb3a637
tigerkaplane <husniyeerparun@gmail.com>
2024-03-26 04:45:15 +0000

    add function

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,6 @@ import { Route } from "react-router-dom";
 function App() {
   return (
     <div>
-
-    
-        <Routes>
-          <Route path="/" element={<Home />} />
-
-        </Routes>
-
     </div>
   );
 }
