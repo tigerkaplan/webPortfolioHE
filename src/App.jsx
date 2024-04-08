24280ecc50957cb26899171a337d25d19b62f710
tigerkaplane <husniyeerparun@gmail.com>
2024-04-08 12:17:53 +0100

    added to router

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import Navbar from "./components/Navbar/Navbar";
+import Home from "./pages/Home";
+import About from "./pages/About";
 
 function App() {
   return (
     <div>
       <Navbar />
+      <Home />
+      <About />
     </div>
   );
 }
