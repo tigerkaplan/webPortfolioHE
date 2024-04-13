32e706875a30499ca6bd5b3534e1761964f41a63
tigerkaplane <husniyeerparun@gmail.com>
2024-04-13 11:36:54 +0100

    app updated

--- src/App.jsx
+++ src/App.jsx
@@ -13,9 +13,7 @@ const App = () => {
 
 <Navbar/>
   <section style={{scrollBehavior: "smooth"}}>
-    
-
-    <Outlet />
+        <Outlet />
   </section>
 
 </div>
--- src/main.jsx
+++ src/main.jsx
@@ -1,31 +1,35 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom" 
-
-// import Pages
-
+import App from './App.jsx';
+import Home from './pages/Home.jsx';
+import About from './pages/About.jsx';
+import Experience from './pages/Experience/Experience.jsx';
+import Features from './pages/Features/Features.jsx';
+import Portfolio from './pages/Portfolio/Portfolio';
+import Blog from './pages/Blog/Blog.jsx';
+import Contact from './pages/Contact.jsx';
 
 const router = createBrowserRouter([
-  {path: "/",
-element: <App/>,
-children: [
   {
-    path: "/",
-    element : <Home/>
-  }
-]
-},
+    path: '/',
+    element: <App />,
+    children: [
+      { path: '/', element: <Home /> },
+      { path: '/about', element: <About /> },
+      { path: '/experience', element: <Experience /> },
+      { path: '/features', element: <Features /> },
+      { path: '/portfolio', element: <Portfolio /> },
+      { path: '/blog', element: <Blog /> },
+      { path: '/contact', element: <Contact /> },
+    ],
+  },
 ]);
-
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router= {router} />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
