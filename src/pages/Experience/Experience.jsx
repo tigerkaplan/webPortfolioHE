f1dbbb52df3c13176775fed69379b515a9f9967c
tigerkaplane <husniyeerparun@gmail.com>
2024-04-29 12:19:00 +0100

    updated

--- src/pages/Experience/Experience.jsx
+++ src/pages/Experience/Experience.jsx
@@ -1,9 +1,100 @@
-import React from 'react'
+import { useState, useEffect } from 'react';
+import experienceData from  '../../assets/data/experienceData.json';
+import ExperienceModal from './ExperienceModal';
 
 const Experience = () => {
-  return (
-    <div>Experience</div>
-  )
-}
+  const [experiences, setExperiences] = useState(experienceData);
+  const [selectTab, setSelectTab] = useState('All');
+  const [showModal, setShowModal] = useState(false); // Set initial state to false
+  const [activeID, setActiveID] = useState(null);
+  const [nextItems, setNextItems] = useState(3); // Initialize nextItems state
 
-export default Experience
+  const loadMoreHandler = () => {
+    setNextItems(prev => prev + 2);
+  };
+
+  const showModalHandler = id => {
+    console.log("Clicked ID:", id);
+    setShowModal(true);
+    setActiveID(id);
+  };
+
+  useEffect(() => {
+    let filteredData = experienceData;
+
+    if (selectTab !== 'All') {
+      filteredData = experienceData.filter(experience => experience.category === selectTab);
+    }
+
+    setExperiences(filteredData);
+  }, [selectTab]);
+
+  return (
+    <div name="experience" className='w-full h-full min-h-screen bg-[#DDD0C8] text-[#282828]'>
+      <div className='max-w-[850px] mx-auto p-4 flex flex-col justify-center items-center w-full h-full'>
+        <div className='mt-[90px] max-w-[1000px]' style={{ width: "80%" }}>
+          <div className='w-full px-4 grid grid-cols-2 gap-8'>
+            <div className='sm:text-right pb-8 pl-4'>
+              <p className='text-[#402b4c] text-3xl font-bold inline border-b-4 border-[#44195e] sm:text-right '>Experience</p>
+            </div>
+          </div>
+          {/* categories */}
+          <div className='inline-flex flex-wrap py-1 sm:px-1 sm:space-x-1  rounded text-sm'>
+            <button type="button" onClick={() => setSelectTab('All')} className='w-full sm:w-auto mb-1 sm:mb-0 mx-1 sm:mx-0 py-2 px-4 hover:bg-pink-600 text-gray-500 hover:text-white rounded hover:shadow font-bold focus:outline-none transition duration-200'>All Technologies</button>
+            <button type="button" onClick={() => setSelectTab('Languages')} className='w-full sm:w-auto mb-1 sm:mb-0 mx-1 sm:mx-0 py-2 px-4 hover:bg-pink-600 text-gray-500 hover:text-white rounded hover:shadow font-bold focus:outline-none transition duration-200'>Languages</button>
+            <button type="button" onClick={() => setSelectTab('Libraries')} className='w-full sm:w-auto mb-1 sm:mb-0 mx-1 sm:mx-0 py-2 px-4 hover:bg-pink-600 text-gray-500 hover:text-white rounded hover:shadow font-bold focus:outline-none transition duration-200'>Libraries</button>
+            <button type="button" onClick={() => setSelectTab('Frameworks')} className='w-full sm:w-auto mb-1 sm:mb-0 mx-1 sm:mx-0 py-2 px-4 hover:bg-pink-600 text-gray-500 hover:text-white rounded hover:shadow font-bold focus:outline-none transition duration-200'>Frameworks</button>
+          </div>
+
+          {/* filter categories */}
+
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4">
+            {experiences?.slice(0, nextItems).map(experience => (
+              <div key={experience.id} className="shadow-md shadow-[#040c16] hover:scale-110 duration-500 group cursor-pointer relative flex items-center">
+                {/* First box (1/5 width) */}
+                <div className="w-1/5 bg-gray-100">
+                  <div className="h-48 flex items-center justify-center transform -rotate-90 font-bold">
+                    <div className='w-1/5'></div>
+                    {experience.title}
+                  </div>
+                </div>
+
+                {/* Second box (4/5 width) */}
+                <div className="w-4/5">
+                  <img
+                    src={experience.image}
+                    alt={experience.title}
+                    className="w-full h-48 object-scale-down rounded-lg"
+                    onClick={() => showModalHandler(experience.id)}
+                  />
+                  <div className="absolute inset-0 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity mr-10px ml-10px ">
+                    <button
+                      onClick={() => showModalHandler(experience.id)}
+                      className="text-white hover:bg-#FDF8F0 text-[15px] py-2 px-4 rounded-[8px] font-[500] ease-in duration-20 mt-5 mr-10px ml-10px m-20px "
+                    >
+                      View
+                    </button>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+
+          {/* Load More Button */}
+          <div className='text-center mt-6'>
+            {nextItems < experiences.length && experienceData.length > 2 && (
+              <button
+                onClick={loadMoreHandler}
+                className='text-black hover:bg-#FDF8F0 py-2 px-4 rounded [8px] font-[500] ease duration-200'>
+                Load More
+              </button>
+            )}
+          </div>
+        </div>
+      </div>
+      {showModal && <ExperienceModal setShowModal={setShowModal} activeID={activeID} />}
+    </div>
+  );
+};
+
+export default Experience;
---
+++ src/pages/Experience/ExperienceModal.jsx
@@ -0,0 +1,88 @@
+import experienceData from '../../assets/data/experienceData.json';
+import { FaGithub,  FaWifi } from 'react-icons/fa';
+import PropTypes from 'prop-types';
+
+const ExperienceModal = ({ activeID, setShowModal }) => {
+    const experience = experienceData.find(experience => experience.id === activeID);
+
+    // Render modal content if experience is found
+    return (
+        <div className="max-w-screen-xl mx-auto p-5 sm:p-10 md:p-16 ">
+            <div className="max-w-[600px] absolute top-1/2 left-1/2 z-20 bg-[#FDF8F0] rounded-[8px] transform -translate-x-1/2 -translate-y-1/2 overflow-y-auto">
+                <div className="!z-5 relative grid grid-col rounded-[20px] bg-white bg-clip-border p-4 shadow-3xl shadow-shadow-500">
+                    <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t  bg-gray-300  ">
+                        <h1 className="text-2xl font-bold-2 text-center">
+                            Related Projects
+                        </h1>
+                        <button type='button' className='text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm h-8 w-8 ms-auto inline-flex justify-center items-center ' onClick={() => setShowModal(false)}>
+                            <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+                            </svg>
+                            <span className='sr-only'></span>
+                        </button>
+                    </div>
+                    <div className='p-4 md:p-5'>
+                        <p className="mt-2 text-xs ">
+                            Here you can find more details about {experience.title} related projects.
+                        </p>
+
+                        <hr className='border-[2px] mt-2' />
+
+
+                        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-2 mt-5">
+
+                            {experience.relatedProjects.map((project, index) => (
+                                <div key={index} className="rounded overflow-hidden shadow-lg flex flex-col">
+                                    <a href="#"> </a>
+                                    <div className="relative">
+                                        <a href="#">
+                                            <img src={project.projectImage} alt={`Project ${index + 1}`} />
+                                            <div className="hover:bg-transparent transition duration-300 absolute bottom-0 top-0 right-0 left-0 bg-gray-900 opacity-25">
+                                            </div>
+                                        </a>
+                                        <a href="#">
+                                            <div className="text-xs absolute top-0 right-0 bg-indigo-600 px-4 py-2 text-white mt-3 mr-3 hover:bg-white hover:text-indigo-600 transition duration-500 ease-in-out">?
+                                            </div>
+                                        </a>
+                                    </div>
+
+                                    
+                                    <div className="px-6 py-4 mb-auto">
+                                        <a href="#" className="font-bold text-lg inline-block hover:text-indigo-600 transition duration-500 ease-in-out  mb-2">
+                                            {project.projectTitle}
+                                        </a>
+                                        <p className="text-gray-500 text-xs  whitespace-nowrap">
+                                            {project.description}
+                                        </p>
+                                    </div>
+
+
+                                    <div className="px-6 py-3 flex flex-row items-center justify-between bg-gray-100">
+                                        <span href="#"
+                                            className='py-1 text-xs font-regular text-gray-900 mr-1 flex flex-row items-center'>
+                                            <span className="ml-1"><a href={project.GitHub}> <FaGithub size={25} /> </a></span>
+                                        </span>
+
+
+                                        <span href="#"
+                                            className="py-1 text-xs font-regular text-gray-900 mr-1 flex flex-row items-center">
+                                            <span className="ml-1"><a href={project.siteURL}>  <FaWifi size={25} className="ml-1" /> </a></span>
+                                        </span>
+                                    </div>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    );
+};
+
+ExperienceModal.propTypes = {
+    activeID: PropTypes.string.isRequired,
+    setShowModal: PropTypes.func.isRequired
+};
+
+export default ExperienceModal;
