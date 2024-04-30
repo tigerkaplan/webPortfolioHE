e1d55ed932309239c4775b2007587242e416eee7
tigerkaplane <husniyeerparun@gmail.com>
2024-04-30 21:07:40 +0100

    email-Js applied to contact form

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,16 +1,56 @@
-import { FaLocationArrow, FaPhoneAlt, FaGithub, FaLinkedin } from "react-icons/fa";
+/* eslint-disable react/no-unescaped-entities */
+import { useState } from 'react'
+import { FaGithub, FaLinkedin } from "react-icons/fa";
 import { HiOutlineMail } from "react-icons/hi";
+import emailjs from 'emailjs-com';
 
 
 
 const Contact = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // EmailJs service ID, template ID, and Public Key
+    const serviceId = 'personalPortfolio';
+    const templateId = 'pPortfolio';
+    const publicKey = 'HK0vJts2mRdncIKnX';
+
+
+    // Create a new object that contains dynamic template parameters
+
+    const templateParams = {
+      fromName: name,
+      fromEmail: email,
+      toName: "Husniye",
+      message: message,
+    };
+
+    //Send email using EmailJS
+    emailjs.send(serviceId, templateId, templateParams, publicKey)
+      .then((response) => {
+        console.log('Email sent successfully!', response);
+        setName('');
+        setEmail('');
+        setMessage('');
+      })
+      .catch((error) => {
+        console.error('Error sending email:', error);
+      });
+
+  };
+
   return (
     <div name="contact"
       className="w-full h-screen  bg-[#DDD0C8] flex justify-center items-center relative p-4 t-5 overflow-y-scroll z-0 ">
 
-      <form
+      <form onSubmit={handleSubmit}
         action=""
-        className='flex flex-col max-w-[600px] w-full'
+        className='emailForm flex flex-col max-w-[600px] w-full'
       >
         <div
           className='pb-2'>
@@ -20,11 +60,31 @@ const Contact = () => {
             Submit the form below or send me an email
           </p>
         </div>
-        <input type="text" placeholder='Name' name='name' className=' p-2 text-sm ml- bg-[#e7ddd8]' />
-        <input type="email" placeholder='Email' className="my-4 p-2 bg-[#e7ddd8]" />
-        <textarea name="message" id="" cols="30" rows="5" placeholder='Message' className="p-2 bg-[#e7ddd8] ">
+        <input
+          type="text"
+          placeholder='Your Name'
+          value={name}
+          className=' p-2 text-sm ml- bg-[#e7ddd8]'
+          onChange={(e) => setName(e.target.value)}
+        />
+        <input
+          type="email"
+          placeholder='Email'
+          value={email}
+          className="my-4 p-2 bg-[#e7ddd8]"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <textarea
+          name="message"
+          cols="30"
+          rows="5"
+          placeholder='Message'
+          value={message}
+          className="p-2 bg-[#e7ddd8]"
+          onChange={(e) => setMessage(e.target.value)}
+        >
         </textarea>
-        <button className="py-2 text-4 hover:shadow  focus:outline-none transition duration-200 group hover:bg-[#d3c3b8] hover:border-[#d3c3b8] text-[#282828] shadow-md shadow-[#c0a899] w-full">  Let's Collaborate
+        <button type='submit' className="py-2 text-4 hover:shadow  focus:outline-none transition duration-200 group hover:bg-[#d3c3b8] hover:border-[#d3c3b8] text-[#282828] shadow-md shadow-[#c0a899] w-full">  Let's Collaborate
         </button>
 
         <div className="p-2 w-full pt-8 m border-t border-gray-200 text-center ">
@@ -46,7 +106,7 @@ const Contact = () => {
             <a className="mx-6 my-1" href="https://www.linkedin.com/in/husniyeerparundev/">
               <FaLinkedin size={30} />
             </a>
-           
+
           </span>
         </div>
 
