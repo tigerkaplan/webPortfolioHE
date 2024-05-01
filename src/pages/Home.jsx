fd90ac6f70cff244f7ca02bbe8af93d9cf1d5e43
tigerkaplane <husniyeerparun@gmail.com>
2024-05-01 14:47:55 +0100

    type animation added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
-import { HiArrowNarrowRight } from "react-icons/hi";
+// import { HiArrowNarrowRight } from "react-icons/hi";
 import { NavLink } from "react-router-dom";
+import { TypeAnimation } from 'react-type-animation';
 
 const Home = () => {
   return (
@@ -10,7 +11,23 @@ const Home = () => {
           Husniye Erparun
         </h1>
         <h2 className="text-1xl sm:text-3xl font-bold text-[#402b4c] mt-6">
-          I'm a Web Developer
+          <TypeAnimation
+            sequence={[
+              // Same substring at the start will only be typed out once, initially
+              'I\'m a softwareDeveloper',
+              1000, // wait 1s before replacing "Mice" with "Hamsters"
+              'I\'m a digitalMarketer',
+              1000,
+              'I\'m a graphicDesigner',
+              1000,
+              'I\'m a marblingArtist',
+              1000
+            ]}
+            wrapper="span"
+            speed={50}
+            style={{  display: 'inline-block' }}
+            repeat={Infinity}
+          />
         </h2>
         <p className="py-4 max-w-[700px]">
           I'm a front-end Developer specializing in building (and occasionally
@@ -19,16 +36,14 @@ const Home = () => {
           <div>
             {/* Use NavLink with the 'to' prop */}
             <NavLink to="/portfolio">
-              <button className='text-[#282828] group border-2 border-[#282828] hover:bg-[#9B928C] hover:border-[#9B928C] px-6 py-3 my-2 flex items-center justify-between'>
+              <button className='text-[#282828] group border-2 border-[#282828] hover:bg-[#9B928C] hover:border-[#9B928C] px-6 py-3 my-2 flex items-center justify-between w-full sm:w-auto mb-1 sm:mb-0 mx-1 sm:mx-0  mt-3 rounded hover:shadow font-bold focus:outline-none transition duration-200 group bg-[#d3c3b8] '>
                 View Portfolio
-                <span className="group-hover:rotate-90 duration-300">
+                {/* <span className="group-hover:rotate-90 duration-300">
                   <HiArrowNarrowRight className="ml-4" />
-                </span>
+                </span> */}
               </button>
             </NavLink>
           </div>
-
-    
         </p>
       </div>
     </div>
