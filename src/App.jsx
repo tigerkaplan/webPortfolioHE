2dfb547481a94dbf5719c2962a8a15354edb070e
tigerkaplane <husniyeerparun@gmail.com>
2024-10-21 00:35:59 +0100

    Json updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,36 @@
-import { Outlet } from 'react-router-dom'
-import './App.css'
-
-
-import Navbar from './component/Navbar'
+import { Outlet } from 'react-router-dom';
+import './App.css';
+import Navbar from './component/Navbar';
+import { Helmet } from 'react-helmet';
 
 const App = () => {
-
   return (
     <>
-<div className='mainContent'>
+    <Helmet>
+  <title>Husniye Erparun's Personal Website</title>
+  <meta 
+    name="description" 
+    content="Welcome to Husniye Erparun's personal website. Explore my projects, skills, and experiences in web development, programming, and technology." 
+  />
+  <meta 
+    name="keywords" 
+    content="Husniye Erparun, web development, programming, personal website, portfolio, technology" 
+  />
+  <link 
+    rel="canonical" 
+    href="https://husniyeerparundev.netlify.app/" 
+  />
+</Helmet>
 
-<Navbar/>
-  <section style={{scrollBehavior: "smooth"}}>
-        <Outlet />
-  </section>
 
-
-</div>
+      <div className='mainContent'>
+        <Navbar />
+        <section style={{ scrollBehavior: "smooth" }}>
+          <Outlet />
+        </section>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
